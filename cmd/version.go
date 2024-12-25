0c70cd35523947dfa1ea37cacc343f2b24d71110
Philip Schmidt <philip.schmidt@networkteam.com>
2024-12-25 13:36:19 +0100

    refactor: Fallback to version from build info

--- cmd/version.go
+++ cmd/version.go
@@ -2,14 +2,16 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
+	"runtime/debug"
 
 	"github.com/urfave/cli/v2"
 )
 
 var (
-	Version        = "dev"
-	BuildTimestamp = "unknown"
-	GoVersion      = "unknown"
+	toolVersion    string
+	goVersion      string
+	buildTimestamp string
 )
 
 func VersionCmd() *cli.Command {
@@ -18,10 +20,39 @@ func VersionCmd() *cli.Command {
 		Aliases: []string{"v"},
 		Usage:   "Print version information",
 		Action: func(c *cli.Context) error {
-			fmt.Printf("mjml-dev version %s\n", Version)
-			fmt.Printf("Go version: %s\n", GoVersion)
-			fmt.Printf("Build time: %s\n", BuildTimestamp)
+			version := GetVersionInfo()
+
+			fmt.Printf("Version %s\n", version.toolVersion)
+			fmt.Printf("Go version: %s\n", version.goVersion)
+
+			if len(buildTimestamp) != 0 {
+				fmt.Printf("Build time: %s\n", buildTimestamp)
+			}
 			return nil
 		},
+	}
+}
+
+type VersionInfo struct {
+	toolVersion string
+	goVersion   string
+}
+
+func GetVersionInfo() VersionInfo {
+	if len(toolVersion) != 0 && len(goVersion) != 0 {
+		return VersionInfo{
+			toolVersion: toolVersion,
+			goVersion:   goVersion,
+		}
+	}
+	if info, ok := debug.ReadBuildInfo(); ok {
+		return VersionInfo{
+			toolVersion: info.Main.Version,
+			goVersion:   runtime.Version(),
+		}
+	}
+	return VersionInfo{
+		toolVersion: "(unknown)",
+		goVersion:   runtime.Version(),
 	}
 }
